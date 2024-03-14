cd54dafe922e45acee4dd5070baff3671ba74de9
malak <gramimalak@gmail.com>
2024-03-14 21:23:08 +0100

    add meta

--- app/(site)/head.tsx
+++ app/(site)/head.tsx
@@ -2,9 +2,17 @@ export default function Head() {
   return (
     <>
       <title> Aouini Peinture - L'art de transformer vos espaces</title>
+      <link rel="alternate" href="https://www.aouinipeinture.com" hrefLang="en" />
+      <link rel="alternate" href="https://www.aouinipeinture.fr" hrefLang="fr" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Built with Next.js and TypeScript" />
+      <meta name="description" content="Aouini Peinture offre des services professionnels de peinture intérieure et extérieure, revêtement muraux, et bien plus. Découvrez comment nous pouvons transformer votre espace." />
       <link rel="icon" href="/images/logo/logo-light.png" />
+      <meta name="keywords" content="peinture,peinture France, peinture intérieure, peinture extérieure, revêtement mural, entreprise de peinture, services de peinture" />
+      <meta property="og:title" content="Aouini Peinture - L'art de transformer vos espaces" />
+      <meta property="og:description" content="Découvrez nos services professionnels de peinture et comment nous pouvons aider à transformer votre espace." />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="/images/logo/logo-light.png" /> 
+      <meta property="og:url" content="https://www.Aouinipeinture.fr" />
     </>
   );
 }
