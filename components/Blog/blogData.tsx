fc0a8ddd8a39edd4ddeff13017cbb2d86c8db281
malak <gramimalak@gmail.com>
2024-03-14 07:23:24 +0100

    update

--- components/Blog/blogData.tsx
+++ components/Blog/blogData.tsx
@@ -4,44 +4,44 @@ const BlogData: Blog[] = [
   {
     _id: 1,
     mainImage: "/images/blog/blog-01.jpeg",
-    title: "Free advertising for your online business",
+    title: "Contraste Sophistiqué",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Notre expertise en nuances et contrastes révèle toute sa splendeur dans cet espace d'escalier, grâce à une peinture murale en noir et blanc aux motifs délicats.",
   },
   {
     _id: 2,
     mainImage: "/images/blog/blog-02.jpeg",
-    title: "9 simple ways to improve your design skills",
+    title: "Vitalité en Escalie",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Des couleurs vives qui donnent vie à l'espace, égayant chaque marche.",
   },
   {
     _id: 2,
     mainImage: "/images/blog/blog-03.jpeg",
-    title: "Tips to quickly improve your coding speed.",
+    title: "Nuances Enfantines.",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Un équilibre charmant de gris, turquoise et blanc pour un cocon doux et chaleureux.",
   },
   {
     _id: 1,
     mainImage: "/images/blog/blog-04.jpeg",
-    title: "Free advertising for your online business",
+    title: "Harmonie Intérieure",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Un intérieur qui marie avec finesse le blanc et le noir, créant une ambiance moderne et accueillante.",
   },
   {
     _id: 2,
     mainImage: "/images/blog/blog-05.jpeg",
-    title: "9 simple ways to improve your design skills",
+    title: "Splendeur Extérieure",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Notre travail de peinture extérieure révèle toute la grandeur de cette vaste maison, en parfaite harmonie avec son environnement.",
   },
   {
     _id: 2,
     mainImage: "/images/blog/blog-06.jpeg",
-    title: "Tips to quickly improve your coding speed.",
+    title: "Engagement Personnel.",
     metadata:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit convallis tortor.",
+      "Travail minutieux sur l'extérieur d'une maison, illustrant notre souci du détail et notre passion pour la peinture.",
   },
 ];
 
