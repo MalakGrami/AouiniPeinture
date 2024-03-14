bb3d7ad5522068fe47a98caf344d4da13d6bc097
malak <gramimalak@gmail.com>
2024-03-14 23:33:15 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["localhost"],
+    domains: ['cdn.sanity.io'],
+    
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
       },
     ],
   },
-  // output: 'export',
+  
+  basePath: '/AouiniPeinture',
+  assetPrefix: '/AouiniPeinture/',
+  output: 'export',
 };
 
 module.exports = nextConfig;
